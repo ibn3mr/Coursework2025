29129ee6eec7ff6c167ada1fb9928bf7ec182389
Omar <omartahir7@gmail.com>
2020-11-17 01:56:13 +0000

    Catalog Creation + Controller testing and validation

--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -1,2 +1,6 @@
 module ProductsHelper
+  # Helper Method so you can convert the number to curency function to GBP insteead of USD
+   def number_to_currency_GBP(number)
+    number_to_currency(number, :unit => "£", :separator => ",", :delimiter => ".")
+  end
 end
