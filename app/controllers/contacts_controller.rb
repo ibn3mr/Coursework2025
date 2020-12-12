a1a87986eac2cf7b7cbb0b4f5fe1798fd4c07c24
Omar <omartahir7@gmail.com>
2020-12-12 23:39:37 +0000

    final commit

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -8,7 +8,8 @@ class ContactsController < ApplicationController
     @contact = Contact.new(params[:contact])
     @contact.request = request
     if @contact.deliver
-      flash.now[:notice] = "Your Message sent to our Admin by Email, they will be with you soon"
+      flash.now[:notice] = "Your Message was sent to our Admin by Email, they will be with you soon"
+
     else
       flash.now[:error] = 'Cannot send message.'
       format.html { render :new }
